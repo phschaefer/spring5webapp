771467c6199f26410492a86646aa3254e0a709ec
Philipp <ph.schaefer@protonmail.com>
2019-12-26 22:08:11 +0100

    Add AuthorController and template author

---
+++ src/main/java/de/phschaefer/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package de.phschaefer.spring5webapp.controllers;
+
+import de.phschaefer.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAllAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
